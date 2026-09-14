c43db2d3da09c857e6f577f11f8770f40ad5e67a
lucia8307 <luciabigailchavezsosa@gmail.com>
2026-09-14 17:11:00 -0300

    Crea la activity

---
+++ app/src/main/java/com/ispc/testace/ui/examenes/catalogo/CatalogoActivity.java
@@ -0,0 +1,16 @@
+package com.ispc.testace.ui.examenes.catalogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ispc.testace.R;
+
+public class CatalogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogo);
+    }
+}
